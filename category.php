a41c6596c951938f41b3e51246a6b357ad64b1ac
i6Grja3R <cyktm19814@yahoo.co.jp>
2025-06-13 15:48:27 +0900

    update206

---
+++ category.php
@@ -0,0 +1,117 @@
+<?php
+require_once 'display.php';
+set_template_info();
+?>
+<?php
+if (is_category() && !is_user_logged_in() && !isBot()) : //個別記事 かつ ログインしていない かつ 非ボット
+    category_views_week(); //アクセスをカウントする
+endif;
+?>
+<?php get_header(); ?>
+<!--ここから自作-->
+<?php display_other_template();?>
+<div id="blog-box" class="clearfix">
+  <!-- ブログ開始 -->
+  <div id="main-box">
+    <!-- メインボックス開始 -->
+    <!--メイン左ボックス-->
+    <div id="left-box">
+      <!--週間カテゴリーランキング-->
+      <div class="popular-keywords">
+        <div class="side-title">人気キーワード(pop keywords)</div>
+        <?php display_category_ranking();?> </div>
+        <!--3日間ランキング-->
+        <?php display_3day_ranking();?>
+        <!--最近のコメント-->
+<?php display_comment();?>
+        <!--アーカイブ-->
+        <?php display_archive();?>
+        <p id="sampleOutput"></p>
+      <!-- ▼　RSS記事右 ▼ -->
+      <div id="right-box">
+        <!-- ▼　RSS記事上 ▼ -->
+        <!-- ▼　検索欄 ▼ -->
+         <?php display_search_form();?>
+        <!-- ▼　週間ランキング ▼ -->
+        <?php display_week_ranking();?>
+        <!-- ▼　メインページ ▼ -->
+        <?php
+/*echo __FILE__;
+echo __DIR__;*/
+$categories = get_categories_array();
+$rss_table_name = get_rss_table_name($tn);
+/*var_dump($rss_table_name);*/
+
+//表示設定
+//ページ番号チェック
+$block_per_page = 2; //ページあたりブロック件数
+$limitSect1 = 5; // タイトルのみの件数
+$limitSect2 = 4; // 画像と画像の下にタイトルの件数
+$limitSect3 = 4; // 画像と画像の右にタイトルの件数
+$rss_per_block = $limitSect1 + $limitSect2 + $limitSect3; // ブロックあたりRSS件数
+//RSS読み込み
+$rss_per_page = $block_per_page * $rss_per_block;
+$rss_offset = ($current_page - 1) * $rss_per_page;
+//※テーブル名の変更
+$sql = "SELECT * FROM {$rss_table_name} ORDER BY date DESC LIMIT %d,%d";
+$query = $wpdb->prepare($sql, $rss_offset, $rss_per_page);
+//SQL分実行と結果取得
+$rss_items = $wpdb->get_results($query);
+$group_per_block = 5; //ブロックあたり投稿グループ件数
+//投稿読み込み
+$posts_per_group = $tn; // 投稿グループあたり投稿件数
+$posts_per_page = $block_per_page * $group_per_block * $posts_per_group; // ページあたり投稿件数
+$posts_offset = ($current_page - 1) * $posts_per_page; //投稿オフセット
+/* 検索件数指定取得 */
+$sql = "
+SELECT
+post.*
+FROM
+wp_posts AS post
+INNER JOIN wp_term_relationships
+ON post.id = wp_term_relationships.object_id
+INNER JOIN wp_term_taxonomy
+ON wp_term_taxonomy.term_taxonomy_id = wp_term_relationships.term_taxonomy_id
+INNER JOIN wp_postmeta AS meta
+ON post.ID = meta.post_id
+WHERE wp_term_taxonomy.term_id = %d
+AND post.post_type = 'post'
+AND post.post_status = 'publish'
+AND meta.meta_key = %s
+ORDER BY
+post.post_date DESC
+LIMIT %d,%d
+";
+$query = $wpdb->prepare($sql, $cat, $tk, $posts_offset, $posts_per_page);
+$post_items = $wpdb->get_results($query);
+//表示
+if (1 == $tn) {
+display_rss_post_1();
+} else {
+display_rss_post_2();
+}
+//ページリンク
+$sql = "
+SELECT
+COUNT(*) AS count
+FROM
+wp_posts AS post
+INNER JOIN wp_term_relationships
+ON post.ID = wp_term_relationships.object_id
+INNER JOIN wp_term_taxonomy
+ON wp_term_taxonomy.term_taxonomy_id = wp_term_relationships.term_taxonomy_id
+INNER JOIN wp_postmeta AS meta
+ON post.ID = meta.post_id
+WHERE wp_term_taxonomy.term_id = %d
+AND post.post_type = 'post'
+AND post.post_status = 'publish'
+AND meta.meta_key = %s
+";
+$query = $wpdb->prepare($sql, $cat, $tk);
+$results = $wpdb->get_results($query);
+$post_count = $results[0]->count;
+display_pagenavi();?>
+      </div>
+</div>
+</div>
+<!--ここまで-->
