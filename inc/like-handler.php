9ec600ea252fc503b2177356aadfc9e9e3aa3f09
i6Grja3R <cyktm19814@yahoo.co.jp>
2025-06-21 19:52:31 +0900

    update222

--- inc/like-handler.php
+++ inc/like-handler.php
@@ -4,39 +4,6 @@
 */
 ?>
 <?php
-add_action('wp_ajax_nopriv_toggle_like', 'handle_like_toggle');
-add_action('wp_ajax_toggle_like', 'handle_like_toggle');
-
-function handle_like_toggle()
-{
-    check_ajax_referer('like_nonce', 'nonce');
-
-    $user_id   = $_COOKIE['user_id'] ?? '';
-    $unique_id = $_POST['unique_id'] ?? '';
-
-    if (!$user_id || !$unique_id) {
-        wp_send_json_error(['message' => '不正なデータです']);
-    }
-
-    if (isGood($user_id, $unique_id)) {
-        deleteGood($user_id, $unique_id);
-        $liked = false;
-    } else {
-        insertGood($user_id, $unique_id);
-        $liked = true;
-    }
-
-    // このコードは関係なさそう
-    wp_send_json_success([
-        'liked'  => $liked,
-        'count'  => count(getGood($unique_id))
-    ]);
-}
-
-// AJAXハンドラ登録
-add_action('wp_ajax_handle_like_action', 'handle_like_ajax');
-add_action('wp_ajax_nopriv_handle_like_action', 'handle_like_ajax');
-
 // トグル対応の AJAX 処理
 function handle_like_ajax()
 {
